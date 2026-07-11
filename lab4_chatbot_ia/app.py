0df6634897bdf29d057cc29ed326aa109dda0363
cama-tech <hernanarturo.cama@unmsm.edu.pe>
2026-07-11 10:20:51 -0500

    Update app.py

--- lab4_chatbot_ia/app.py
+++ lab4_chatbot_ia/app.py
@@ -314,4 +314,4 @@ def health():
     return jsonify({"status":"ok","lab":"Laboratorio 4 - IA y OWASP"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
